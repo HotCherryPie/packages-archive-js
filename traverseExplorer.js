93536a3ebeb9ff141e4dceca5ccc0946255d8125
Artem Soldatov <thearteman@gmail.com>
2021-05-31 21:37:28 +0300

    add missing param

--- traverseExplorer.js
+++ traverseExplorer.js
@@ -1,6 +1,6 @@
 function traverse(target, func, path = [], depth = 0) {
   Object.entries(target).forEach(([key, value]) => {
-      func({ key, value, path });
+      func({ key, value, path, depth });
       if (value && typeof (value) === 'object') traverse(value, func, [...path, key], depth + 1);
   });
 }
